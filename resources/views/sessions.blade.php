8bd95da19f31d29b91d93213b2ca95212eaaae7f
«julieSalha» <julie.salha@gmail.com>
2020-05-28 18:24:11 +0200

    update session blade

--- resources/views/sessions.blade.php
+++ resources/views/sessions.blade.php
@@ -1,68 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">Les Sessions de la formation {{$training->name}}:</div>
-            <div class="card-body">
-                <a href="{{ route('add_session', $training->id) }}">Ajouter une nouvelle session</a>
-                <ul>
-                    @foreach ($sessions as $session)
-                    <li> {{$session->name}}
-                        <ul>
-                            <li> {{$session->date}} </li>
-                            <li> {{$session->availables_seats}} </li>
-                            <li> {{$session->configuration}} </li>
-                            <li> {{$session->room->name}} </li>
-                            <li>Professeur : {{$session->report->user->name}} </li>
-                            <li><a href="{{ route('edit_session', $session->id) }}">Editer la session</a></li>
-                            @if($session->report === null)
-                                <li><a href="">Créer un compte-rendu</a> </li>
-                            @else
-                                <li><a href="{{ route('reports', $session->id) }}">Voir le compte-rendu</a></li>
-                            @endif
-                            @if (Auth::user()->role == "user")
-                                @if ($session->grades->isNotEmpty() & Auth::user()->grades->isNotEmpty())
-                                    @foreach( Auth::user()->grades as $grade )
-                        <li> {{$session->name}}
-                            <ul>
-                                <li> {{$session->date}} </li>
-                                <li> {{$session->availables_seats}} </li>
-                                <li> {{$session->configuration}} </li>
-                                <li> {{$session->room->name}} </li>
-                                <li>Professeur : {{$session->report->user->name}} </li>
-                                @if (Auth::user()->role == "user" & $session->grades->isNotEmpty() & Auth::user()->grades->isNotEmpty())
-                                    @if(!Auth::user()->grades->isEmpty())
-                                        @foreach( Auth::user()->grades as $grade )
-                                        @if($grade->session_id == $session ->id)
-                                            <li>Vous êtes déjà inscrit </li>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Les Sessions de la formation {{$training->name}}:</div>
+                <div class="card-body">
+                    <a href="{{ route('add_session', $training->id) }}">Ajouter une nouvelle session</a>
+                    <ul>
+                        @foreach ($sessions as $session)
+                            <li> {{$session->name}}
+                                <ul>
+                                    <li> {{$session->date}} </li>
+                                    <li> {{$session->availables_seats}} </li>
+                                    <li> {{$session->configuration}} </li>
+                                    <li> {{$session->room->name}} </li>
+                                    <li>Professeur : {{$session->report->user->name}} </li>
+                                    <li><a href="{{ route('edit_session', $session->id) }}">Editer la session</a></li>
+                                    @if($session->report->content == null & $session->report->name == null)
+                                        @if(Auth::user()->role == "teacher")
+                                            <li><a href="">Créer un compte-rendu</a> </li>
                                         @endif
-                                    @endforeach
-                                @else
-                                    @if ($session->availables_seats > 0)
-                                        <li> <a href="{{route('registration', $session->id) }}"> Inscription </a> </li>
                                     @else
-                                        <li>Il n'y a plus de place</li>
+                                        <li><a href="{{ route('reports', $session->id) }}">Voir le compte-rendu</a></li>
                                     @endif
-                                @endif
-                            @endif
-                                    <li> <a href="{{route('registration', $session->id) }}"> Inscription </a> </li>
-                                @endif
-
-                                @if($session->report->content == null & $session->report->name == null)
-                                    @if(Auth::user()->role == "teacher")
-                                    <li><a href="">Créer un compte-rendu</a> </li>
+                                    @if (Auth::user()->role == "user")
+                                        @if ($session->grades->isNotEmpty() & Auth::user()->grades->isNotEmpty())
+                                            @foreach( Auth::user()->grades as $grade )
+                                                @if($grade->session_id == $session ->id)
+                                                    <li>Vous êtes déjà inscrit </li>
+                                                @endif
+                                            @endforeach
+                                        @else
+                                            @if ($session->availables_seats > 0)
+                                                <li> <a href="{{route('registration', $session->id) }}"> Inscription </a> </li>
+                                            @else
+                                                <li>Il n'y a plus de place</li>
+                                            @endif
+                                        @endif
                                     @endif
-                                @else
-                                    <li><a href="{{ route('reports', $session->id) }}">Voir le compte-rendu</a></li>
-                                @endif
-                            </ul>
-                    </li>
-                    @endforeach
-                </ul>
+                                </ul>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
